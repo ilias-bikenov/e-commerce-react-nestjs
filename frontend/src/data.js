5633667d0c56dee5e28711ca9c00836d0d4a5e6c
ilias_bikenov <ilias.bikenov@gmail.com>
2022-01-02 19:00:17 +0600

    new commit

---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/1_1.jpeg',
+      price: 120,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'description 1'
+    },
+    {
+      _id: '2',
+      name: 'Slim Shirt 2',
+      category: 'Shirts',
+      image: '/images/1_2.jpeg',
+      price: 150,
+      brand: 'Nike',
+      rating: 2,
+      numReviews: 10,
+      description: 'description 2'
+    },
+    {
+      _id: '3',
+      name: 'Ordinary Shirt ',
+      category: 'Shirts',
+      image: '/images/2_1.jpeg',
+      price: 210,
+      brand: 'Polo',
+      rating: 4.0,
+      numReviews: 14,
+      description: 'description 3'
+    },
+  ]
+}
+
+export default data
