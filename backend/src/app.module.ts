4ba2b6bb8448d9355df229001684d3e188bd446a
ilias_bikenov <ilias.bikenov@gmail.com>
2022-02-14 17:18:16 +0600

    add docker support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -24,7 +24,7 @@ import { AuthModule } from './auth/auth.module';
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT) || 5432,
       username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      password: process.env.POSTGRES_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadModels: true,
       synchronize: true,
